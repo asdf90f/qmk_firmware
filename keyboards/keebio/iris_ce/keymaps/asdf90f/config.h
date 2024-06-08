abb92c0af600e5aa8aa909f21a6d01c88c95170f
asdf90f <50677687+asdf90f@users.noreply.github.com>
2024-06-08 10:53:20 -0700

    added non-hrm keymap iris ce

--- keyboards/keebio/iris_ce/keymaps/asdf90f/config.h
+++ keyboards/keebio/iris_ce/keymaps/asdf90f/config.h
@@ -21,5 +21,5 @@
 #define MOUSEKEY_TIME_TO_MAX    64
 #define MOUSEKEY_MAX_SPEED      6
 #define MOUSEKEY_WHEEL_DELAY 30
-#define TAPPING_TERM    200
+#define TAPPING_TERM    150
 #define QUICK_TAP_TERM 0
